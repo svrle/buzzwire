3357c62db96cd3babfd619330bfcda2728178596
Grgic Nenad <gnenad>
2023-12-31 12:44:46 +0100

    7segment display initial

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 
 #define CONTACT_DELAY 500
 
+char point[10] = {95, 3, 214, 220, 153, 205, 207, 88, 223, 221};
+
 ISR( INT1_vect ) {
   cli();
   PORTB |= (1 << PB0);	// pin on 0 of port B
@@ -16,6 +18,11 @@ ISR( INT1_vect ) {
 int main (void)
 {
     DDRB |= (1 << DDB0);      // set pin 0 of port B as an output pin
+    /**
+     * Enable pins for SN74CH595N
+     */
+    DDRC |= (1 << DDC3);      // set pin 3 of port C as an output pin SER
+    DDRC |= (1 << DDC2);      // set pin 2 of port C as an output pin SRCLK RCLK
 
     /**
      * Enable interrupt 
@@ -25,12 +32,28 @@ int main (void)
     DDRD &= ~(1<<PD3);			// INT1 input
     PORTD = (1<<PD3);			// INT1 pull-up 
     sei();				// enable interrupt routine
-    /* Enable interruptt */
+    /* Enable interrupt */
 
     // Set by default PB0 low
     DDRB |= ~(1<<DDB0);
+
     while (1) {
-      //wheel loop
+      /**
+       * 7Segment display driven by SN74CH595N
+       */
+      char i;
+      for(i=0; i<8; i++) {
+      	PORTC &= ~(1 << PC2); 	// Clock low
+        			// If bit in number is 1 send 1 to SER, if it's 0 send 0 to SER
+        if(point[0] & (1 << i)) {
+	  PORTC |= (1 << PC3);
+	} else {  
+          PORTC &= ~(1 << PC3);
+	}
+      	PORTC |= (1 << PC2); 	// Clow high
+      }
+      /* 7Segment display driven by SN74CH595N */
+      _delay_ms(1000);
     }
 
 }
